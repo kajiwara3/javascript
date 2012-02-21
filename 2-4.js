6dfe4d1a31a533e07a3f5822f25d8d58902f78dd
kaji3 <kajiwara3@gmail.com>
2012-02-22 00:18:38 +0900

    2-4に処理を追加した

--- 2-4.js
+++ 2-4.js
@@ -1,15 +1,23 @@
-$(function(){
-    $("input").val("入力してください").css("color", "#ccc").one("focus", function(){
+$(function() {
+    $("select").change(function() {
+        if ($(":selected").attr("value") == "10") {
+            $("input").attr("disabled", "disabled");
+        } else {
+            $("input").removeAttr("disabled");
+        }
+    });
+/*
+    $("input").val("入力してください").css("color", "#ccc").one("focus", function() {
         $(this).val("").css("color", "#000");
-    }).blur(function(){
+    }).blur(function() {
         if ($(this).val() == "") {
-            $(this).val("入力してください").css("color", "#ccc").one("focus", function(){
+            $(this).val("入力してください").css("color", "#ccc").one("focus", function() {
                 $(this).val("").css("color", "#000");
             });
         }
     });
-
-    $("button").click(function(){
+*/
+    $("button").click(function() {
         $("input").val("");
     });
 });
